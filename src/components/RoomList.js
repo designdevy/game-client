cf1cbc42cabc4560743389a9a4e5e085a6b681e4
nataliaev <nataliiaevchenko@gmail.com>
2019-08-12 17:42:46 +0200

    Add joining the room

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { allRooms } from "../actions";
 import { connect } from "react-redux";
+import superagent from "superagent";
 
 class RoomList extends React.Component {
+  handleClick = async (event) => {
+    event.preventDefault();
+    console.log('event.target.value', event.target.value)
+
+    const serverUrl =
+      "https://polar-sands-55886.herokuapp.com" ||
+      "http://localhost:5000";
+
+    await superagent
+    .put(`${serverUrl}/rooms/${event.target.value}`)
+    .send({
+      roomId: event.target.value,
+      userId: this.props.user.id
+    })
+  };
+
   render() {
     const rooms = this.props.rooms.map(room => (
       <div key={room.id}>
         <p>
           {room.name} status: {room.status}
         </p>
-        <button onClick={this.onClick}>Join</button>
+        <button value={room.id} onClick={this.handleClick}>Join</button>
       </div>
     ));
 
