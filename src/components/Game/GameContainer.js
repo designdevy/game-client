7ce5f3b93d9abf92e4e77a6802ab06bafa2fa8b0
nataliaev <nataliiaevchenko@gmail.com>
2019-08-14 16:51:45 +0200

    Add shapes answers

--- src/components/Game/GameContainer.js
+++ src/components/Game/GameContainer.js
@@ -4,30 +4,68 @@ import { connect } from "react-redux";
 import superagent from "superagent";
 import { serverUrl } from "../serverUrl";
 import { withRouter, Redirect } from "react-router-dom";
+import "./Game.css";
 
 class GameContainer extends React.Component {
   state = {
-    redirect: false
-  }
+    redirect: false,
+    values: [1, 2, 3, 4, 5]
+  };
 
   handleChoice = async event => {
     event.preventDefault();
+
+    console.log("event.target.value", event.currentTarget.value);
 
     const room = this.props.rooms.find(
       room => parseInt(room.id) === parseInt(this.props.match.params.id)
     );
 
     await superagent.post(`${serverUrl}/choice`).send({
-      value: event.target.value,
+      value: event.currentTarget.value,
       userId: this.props.user.id,
       roomId: room.id
     });
+
+    if (parseInt(room.stage) >= 7) {
+      this.setState({
+        values: [
+          <img
+            className="figure"
+            src="https://cdn3.iconfinder.com/data/icons/eldorado-stroke-symbols/40/shape_circle-512.png"
+            alt="figure"
+          />,
+          <img
+            className="figure"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Regular_triangle.svg/400px-Regular_triangle.svg.png"
+            alt="figure"
+          />,
+          <img
+            className="figure"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/500px-Square_-_black_simple.svg.png"
+            alt="figure"
+          />,
+          <img
+            className="figure"
+            src="https://cdn0.iconfinder.com/data/icons/symbols-symbols-add-on-3-vol-1/48/v-26-512.png"
+            alt="figure"
+          />,
+          <img
+            className="figure"
+            src="https://cdn4.iconfinder.com/data/icons/line-icons-12/64/software_shape_oval-512.png"
+            alt="figure"
+          />
+        ]
+      });
+    }
+
+
   };
 
   // remove the user to the current room, if user quit the game
   quitGame = async event => {
     event.preventDefault();
-    this.setState({redirect: true})
+    this.setState({ redirect: true });
     await superagent.put(`${serverUrl}/users/${this.props.user.id}`).send({
       roomId: this.props.match.params.id,
       userId: this.props.user.id
@@ -36,9 +74,9 @@ class GameContainer extends React.Component {
 
   renderRedirect = () => {
     if (this.state.redirect === true) {
-      return <Redirect to={'/'} />;
+      return <Redirect to={"/"} />;
     } else {
-      return <div/>;
+      return <div />;
     }
   };
 
@@ -53,6 +91,7 @@ class GameContainer extends React.Component {
           room={room}
           onClick={this.handleChoice}
           quitGame={this.quitGame}
+          values={this.state.values}
         />
         {this.renderRedirect()}
       </div>
--- src/components/Game/GameDisplay.js
+++ src/components/Game/GameDisplay.js
@@ -1,6 +1,102 @@
 import React from "react";
 import "./Game.css";
 
+function Victory () {
+  return <div>
+    <h2>Congratulations! You are a wonderful team!</h2>
+  </div>
+}
+
+function Failure () {
+  return <div>
+    <h2>You loose... Try one more time</h2>
+  </div>
+}
+
+function Waiting ({ on }) {
+  return <h3>Waiting for the {on}.</h3>
+}
+
+function Button ({ onClick, value, content }) {
+  return <button
+    className="answer-button"
+    value={value}
+    onClick={onClick}
+  >
+    {content}
+  </button>
+}
+
+function History ({ round, other, previous }) {
+  return round > 1
+    ? <h2>
+      The other player answered {other.value} and you answered{" "}
+      {previous.value} in the previous round
+    </h2>
+    : <h2>Give your answer! Will your partner match?</h2>
+}
+
+function Choices ({ onClick, room, values, styles, other, previous }) {
+  console.log('values test:', values)
+
+  return <div>
+    <h3>
+      Try to choose the same answer as your game partner, then you will
+      get level up, if not - you will loose your points
+    </h3>
+
+    <h2>Choose your answer:</h2>
+
+    <div className="answers-container">
+      <Button value="1" content={values[0]} onClick={onClick} />
+      <Button value="2" content={values[1]} onClick={onClick} />
+      <Button value="3" content={values[2]} onClick={onClick} />
+      <Button value="4" content={values[3]} onClick={onClick} />
+      <Button value="5" content={values[4]} onClick={onClick} />
+    </div>
+
+    <h2>Your level is {room.stage}</h2>
+
+    <h2>It's round #{room.round}</h2>
+
+    <div className="container">
+      <div className="progress-bar" style={styles} />
+    </div>
+
+    <History round={room.round} other={other} previous={previous} />
+  </div>
+}
+
+function Content ({ room, user, onClick, values, styles, other, previous }) {
+  if (room.users.length === 2) {
+    if (room.stage === 10) {
+      return <Victory />
+    } 
+    
+    if (room.stage === 0) {
+      return <Failure />
+    }
+
+    const choice = room.choices &&
+      room.choices.find(choice => choice.round === room.round && choice.userId === user.id)
+
+    if (choice) {
+      return <Waiting on="other player's choice" />
+    }
+    
+    return <Choices
+      onClick={onClick}
+      room={room}
+      values={values}
+      styles={styles}
+      other={other}
+      previous={previous}
+    />
+  }
+    
+  return <Waiting on="other player to join" />
+}
+
 export default function GameDisplay(props) {
   const previousAnswer = props.room.choices.find(
     choice =>
@@ -11,8 +107,8 @@ export default function GameDisplay(props) {
     choice =>
       choice.round === props.room.round - 1 && choice.userId !== props.user.id
   );
-  
-  let height = 350 - (parseInt(props.room.stage) * 35)
+
+  let height = 350 - parseInt(props.room.stage) * 35;
 
   let styles = {
     width: "150px",
@@ -20,54 +116,20 @@ export default function GameDisplay(props) {
     backgroundColor: "white"
   };
 
-  let styles2 = {
-    width: "150px",
-    height: "350px",
-    backgroundImage: "linear-gradient(to bottom, #27ae60,#f1c40f,#c0392b)"
-  }
-
   return (
     <div className="hole-game">
       <h1>Game:</h1>
-      {props.room.users.length === 2 ? (
-        props.room.stage === 10 ? (
-          <div>
-            <h2>Congratulations! You are a wonderful team!</h2>
-          </div>
-        ) : props.room.stage === 0 ? (
-          <div>
-            <h2>You loose... Try one more time</h2>
-          </div>
-        ) : (
-          <div>
-            <h2>Choose your answer:</h2>
-            <button value="1" onClick={props.onClick}>
-              1
-            </button>
-            <button value="2" onClick={props.onClick}>
-              2
-            </button>
-            <button value="3" onClick={props.onClick}>
-              3
-            </button>
-            <h2>Your level is {props.room.stage}</h2>
-            <h2>It's round #{props.room.round}</h2>
-            <div className="container" style={styles2}>
-              <div className="progress-bar" style={styles} />
-            </div>
-            {props.room.round > 1 ? (
-              <h2>
-                The other player answered {otherAnswer.value} and you answered{" "}
-                {previousAnswer.value} in the previous round
-              </h2>
-            ) : (
-              <p>Waiting for the answers...</p>
-            )}
-          </div>
-        )
-      ) : (
-        <p>Waiting for the other player</p>
-      )}
+      
+      <Content
+        room={props.room}
+        user={props.user}
+        onClick={props.onClick}
+        values={props.values}
+        styles={styles}
+        other={otherAnswer}
+        previous={previousAnswer}
+      />
+
       <button onClick={props.quitGame}>Quit the game</button>
     </div>
   );
