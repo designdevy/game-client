fd1bf3737305c0bf392c0c5abf1374d402aecfaa
nataliaev <nataliiaevchenko@gmail.com>
2019-08-16 11:35:11 +0200

    Add all serverUrl in one file

--- src/components/serverUrl.js
+++ src/components/serverUrl.js
@@ -1,4 +1,2 @@
-// export const serverUrl =
-// "https://polar-sands-55886.herokuapp.com"
-
 export const serverUrl = "http://localhost:5000";
+// "https://polar-sands-55886.herokuapp.com" || "http://localhost:5000"
